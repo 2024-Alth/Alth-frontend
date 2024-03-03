89e7b6e640b223a4ed750668c6634741d43fb414
BellYun <95991290+BellYun@users.noreply.github.com>
2024-03-04 02:39:14 +0900

    feat : 사용자 기록 캘린더에 기록

--- src/components/Main/Calendar.tsx
+++ src/components/Main/Calendar.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Calendar from "react-calendar";
 import "../../../node_modules/react-calendar/src/Calendar.css";
 import moment from "moment";
+import axios from "axios";
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -10,7 +11,28 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 function Calender() {
   const [value, onChange] = useState<Value>(new Date());
 
-  //const [dateList, onChangedateList] = useState<Value[]>([]);
+  // const [dateList, onChangedateList] = useState<Value[]>([]);
+  // const [dateDataList, onChangedateDataList] = useState<Value[]>([]);
+
+  // useEffect(() => {
+  //   GetDate();
+  // }, []);
+
+  // const GetDate = async () => {
+  //   try {
+  //     const response = await axios({
+  //       method: "get",
+  //       url: "http://localhost:8080/user/record/all",
+  //       headers: { Authorization: `${localStorage.getItem("user")}` },
+  //     });
+  //     console.log(response.data.recordList);
+  //     const data = response.data.recordList;
+  //     const listData = data.map((item) => item.recordDate);
+  //     onChangedateList(listData);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   const dateList = ["2024-02-04", "2024-03-04"];
 
@@ -55,7 +77,6 @@ const Layout = styled.div`
   border-radius: 20px;
   margin-top: 16px;
   margin-bottom: 16px;
-  position: relative;
 `;
 
 const StyledCalendarWrapper = styled.div`
@@ -63,14 +84,15 @@ const StyledCalendarWrapper = styled.div`
   display: flex;
   justify-content: center;
 `;
-export const StyledCalendar = styled(Calendar)``;
+export const StyledCalendar = styled(Calendar)`
+  position: relative;
+`;
 
 export const StyledDot = styled.div`
   background-color: red;
   border-radius: 50%;
   width: 0.3rem;
   height: 0.3rem;
-  position: absolute;
   top: 60%;
   left: 50%;
   transform: translateX(-50%);
