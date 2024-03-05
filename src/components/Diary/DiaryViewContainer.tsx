d0643a0f2960ffde909a2f3c19b17c88a8b177d9
BellYun <whd3558@naver.com>
2024-03-05 21:58:07 +0900

    feat : 임시 record 조회 생성

--- src/components/Diary/DiaryViewContainer.tsx
+++ src/components/Diary/DiaryViewContainer.tsx
@@ -1,13 +1,64 @@
-import styled from "styled-components"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
-function DiaryViewContainer(){
-  return(
-    <Layout>
-    </Layout>
-  )
+interface DiaryViewContainerProps {
+  date: string;
+  dateList: string[];
+  index: number[];
 }
 
-export default DiaryViewContainer
+function DiaryViewContainer({
+  date,
+  dateList,
+  index,
+}: DiaryViewContainerProps) {
+  const [log, setLog] = useState([]);
+
+  useEffect(() => {
+    if (dateList.includes(date)) {
+      const dateIndex = dateList.indexOf(date);
+      const targetIndexValue = index[dateIndex];
+      console.log("Target index value:", targetIndexValue);
+      GetDate(targetIndexValue);
+    }
+  }, [date, dateList, index]);
+
+  const GetDate = async (index: number) => {
+    try {
+      const response = await axios({
+        method: "get",
+        url: `http://localhost:8080/user/record/${index}`,
+        headers: { Authorization: `${localStorage.getItem("user")}` },
+      });
+      console.log(response.data);
+      setLog(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Layout>
+      <div>{log?.recordDate}</div>
+      <div>내 상태는? {log?.hangOver}</div>
+      <div>음주 기록</div>
+      {log?.alcohols?.map((index) => (
+        <div>
+          <div>
+            {index?.alcoholName}
+            {index?.alcoholType}{" "}
+          </div>
+          <div>
+            {index?.degree}도 {index?.volume}ml{" "}
+          </div>
+        </div>
+      ))}
+    </Layout>
+  );
+}
+
+export default DiaryViewContainer;
 
 const Layout = styled.div`
   width: 100%;
@@ -17,6 +68,6 @@ const Layout = styled.div`
   flex-direction: column;
   align-items: center;
   border-radius: 20px;
-  margin-top:16px;
+  margin-top: 16px;
   margin-bottom: 16px;
-`
+`;
